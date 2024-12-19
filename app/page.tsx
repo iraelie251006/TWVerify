3043f0c68b5fef4a0f62b09a28aa0e9a851f4b73
Elie <niyubwayoiraelie5777@gmail.com>
2024-12-19 21:42:41 +0200

    Modify Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="text-center">
       <h1 className="font-bold text-center mb-5 text-4xl">
-        How To Make One Time Password Phone Authentication
+        How To Make One Time Password Phone Authentication using Firebase
       </h1>
     </main>
   );
